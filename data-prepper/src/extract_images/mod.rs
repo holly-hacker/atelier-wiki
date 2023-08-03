157b6a6763ae1f9a3995d38afb15cec7137269a4
HoLLy <dario.db@hotmail.com>
2023-08-04 00:22:08 +0200

    Add cli option for lossless png compression using oxipng

--- data-prepper/src/extract_images/mod.rs
+++ data-prepper/src/extract_images/mod.rs
@@ -17,6 +17,10 @@ pub struct Args {
     /// the output directory
     #[argh(option, short = 'o')]
     output_directory: Option<PathBuf>,
+
+    /// level of oxipng compression to use. if not present, use standard png encoder
+    #[argh(option, short = 'c')]
+    compression: Option<u8>,
 }
 
 impl Args {
@@ -43,29 +47,41 @@ impl Args {
         let mut pak_index = PakIndex::read(&pak_dir, game_version).context("read data dir")?;
         info!("Loaded pak file index with {} entries", pak_index.len());
 
-        info!("Extracting monster portraits");
-        const MONSTER_PATTERN: &str = r"\data\x64\res_cmn\ui\neo\neo_a24_monster_l_*.g1t";
-        let monsters_path = output_directory.join("monsters");
-        extract_prefixed(&mut pak_index, MONSTER_PATTERN, &monsters_path)
-            .context("extract monster portraits")?;
-
-        info!("Extracting item icons");
-        const ITEM_PATTERN: &str = r"\data\x64\res_cmn\ui\neo\neo_a24_item_l_*.g1t";
-        let items_path = output_directory.join("items");
-        extract_prefixed(&mut pak_index, ITEM_PATTERN, &items_path)
-            .context("extract item icons")?;
+        extract_images(&mut pak_index, &output_directory, self.compression)?;
+        info!("Extracted images");
 
         Ok(())
     }
 }
 
+fn extract_images(
+    pak_index: &mut PakIndex,
+    output_directory: &Path,
+    opt_level: Option<u8>,
+) -> anyhow::Result<()> {
+    info!("Extracting monster portraits");
+    const MONSTER_PATTERN: &str = r"\data\x64\res_cmn\ui\neo\neo_a24_monster_l_*.g1t";
+    let monsters_path = output_directory.join("monsters");
+    extract_prefixed(pak_index, MONSTER_PATTERN, &monsters_path, opt_level)
+        .context("extract monster portraits")?;
+
+    info!("Extracting item icons");
+    const ITEM_PATTERN: &str = r"\data\x64\res_cmn\ui\neo\neo_a24_item_l_*.g1t";
+    let items_path = output_directory.join("items");
+    extract_prefixed(pak_index, ITEM_PATTERN, &items_path, opt_level)
+        .context("extract item icons")?;
+
+    Ok(())
+}
+
 fn extract_prefixed(
     pak_index: &mut PakIndex,
     pattern: &'static str,
-    output_path: &Path,
+    output_folder: &Path,
+    opt_level: Option<u8>,
 ) -> anyhow::Result<()> {
     debug!("Creating output directory");
-    std::fs::create_dir_all(output_path).context("create output directory")?;
+    std::fs::create_dir_all(output_folder).context("create output directory")?;
 
     let mut entries: Vec<_> = pak_index
         .iter_entries()
@@ -83,6 +99,7 @@ fn extract_prefixed(
             .with_context(|| format!("read {entry}"))?
             .with_context(|| format!("cannot find entry {entry}"))?;
 
+        debug!(?entry, "reading g1t header");
         let g1t = gust_g1t::GustG1t::read(&mut file).context("read g1t")?;
         let texture = &g1t.textures[0];
 
@@ -94,17 +111,51 @@ fn extract_prefixed(
             );
         }
 
+        debug!(?entry, "reading image");
         let image_bytes = g1t.read_image(texture, &mut file).context("read image")?;
 
-        let image_buffer = image::RgbaImage::from_vec(texture.width, texture.height, image_bytes)
-            .context("image to rgbimage vec")?;
+        debug!(?entry, "converting image");
 
-        let output_path = output_path.join(format!("{monster_num}.png"));
+        debug!(?entry, "encoding image to png...");
+        let png_bytes = if let Some(compression) = opt_level {
+            let image_buffer = oxipng::RawImage::new(
+                texture.width,
+                texture.height,
+                oxipng::ColorType::RGBA,
+                oxipng::BitDepth::Eight,
+                image_bytes,
+            )
+            .context("load raw buffer as image")?;
+            let mut opts = oxipng::Options::from_preset(compression);
 
-        debug!("saving image...");
-        image_buffer
-            .save_with_format(output_path, image::ImageFormat::Png)
-            .context("save file")?;
+            // explicitly allow modifying alpha, which gives another ~7% improvement on level 1
+            opts.optimize_alpha = true;
+
+            image_buffer
+                .create_optimized_png(&opts)
+                .context("optimize png")?
+        } else {
+            let mut png_bytes = vec![];
+
+            let mut encoder = png::Encoder::new(&mut png_bytes, texture.width, texture.height);
+            encoder.set_color(png::ColorType::Rgba);
+            encoder.set_depth(png::BitDepth::Eight);
+            encoder.set_adaptive_filter(png::AdaptiveFilterType::Adaptive);
+
+            let mut writer = encoder.write_header().context("write png header")?;
+            writer
+                .write_image_data(&image_bytes)
+                .context("write png data")?;
+
+            drop(writer);
+
+            png_bytes
+        };
+
+        let file_path = output_folder.join(format!("{monster_num}.png"));
+        debug!(?entry, ?file_path, "saving image...");
+        std::fs::write(&file_path, png_bytes).context("write to png file")?;
+        debug!(?entry, ?file_path, "saved image");
     }
 
     Ok(())
