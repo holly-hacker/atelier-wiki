46a0bf54e59d8635aefc73edc6da9d372fe92718
HoLLy <dario.db@hotmail.com>
2023-10-29 00:08:47 +0200

    Show puni feeding source, if applicable

--- frontend/src/routes/ryza3/items/detail/sources.tsx
+++ frontend/src/routes/ryza3/items/detail/sources.tsx
@@ -1,46 +1,38 @@
 import types from "@/data/types/ryza3";
 import { EnemyLink } from "../../utility_components/links";
 import { useContext } from "react";
-import { Ryza3Context, Ryza3Data } from "@/data/ryza3_data";
+import { Ryza3Context } from "@/data/ryza3_data";
+import { Link } from "react-router-dom";
 
 export function ItemDropSourcesSection({ item }: { item: types.Item }) {
   const ryza3Data = useContext(Ryza3Context);
-  const drops = getDrops(ryza3Data, item);
+
+  const monsters = MonsterSources(item);
+  const puniFeeding = PuniFeedingSources(item);
+
+  const noKnownSources = !monsters && !puniFeeding;
+  const canBeCrafted = ryza3Data.recipes.recipes.some(
+    (r) => r.item_tag == item.tag,
+  );
 
   return (
     <>
-      <h2>Monster drops</h2>
-      {drops.length > 0 ? (
-        <>
-          <ul>
-            {drops.map(({ drop, status, enemy }, i) => {
-              return (
-                <li key={i}>
-                  <EnemyLink enemy={enemy}>
-                    {enemy.name} (lv {status.lv})
-                  </EnemyLink>
-                  : Drop rate: {drop.num}x {drop.rate}%
-                </li>
-              );
-            })}
-          </ul>
-          <details>
-            <summary>Json data</summary>
-            <pre>{JSON.stringify(drops, null, 4)}</pre>
-          </details>
-        </>
-      ) : (
-        <p>This item does not drop from monsters.</p>
+      <h2>Sources</h2>
+      {monsters}
+      {puniFeeding}
+
+      {noKnownSources && (
+        <p>
+          There are no known sources for this item
+          {canBeCrafted && " besides crafting"}.
+        </p>
       )}
     </>
   );
 }
 
-function getDrops(
-  ryza3Data: Ryza3Data,
-  item: types.Item,
-): { drop: types.EnemyDrop; status: types.EnemyStatus; enemy: types.Enemy }[] {
-  if (!item.tag) return [];
+function MonsterSources(item: types.Item) {
+  const ryza3Data = useContext(Ryza3Context);
 
   const drops = [];
 
@@ -54,5 +46,67 @@ function getDrops(
     }
   }
 
-  return drops;
+  if (drops.length == 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <h3>Monster drops</h3>
+      <ul>
+        {drops.map(({ drop, status, enemy }, i) => {
+          return (
+            <li key={i}>
+              <EnemyLink enemy={enemy}>
+                {enemy.name} (lv {status.lv})
+              </EnemyLink>
+              : Drop rate: {drop.num}x {drop.rate}%
+            </li>
+          );
+        })}
+      </ul>
+      <details>
+        <summary>Json data</summary>
+        <pre>{JSON.stringify(drops, null, 4)}</pre>
+      </details>
+    </>
+  );
+}
+
+function PuniFeedingSources(item: types.Item) {
+  const ryza3Data = useContext(Ryza3Context);
+
+  const events = ryza3Data.puni_feeding.unique_events;
+  const filteredEvents = events.filter((e) => e.item_tag == item.tag);
+
+  if (filteredEvents.length == 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <h3>Puni feeding</h3>
+      {filteredEvents.length == 1 && (
+        <>
+          <p>
+            This item drops as a special item through puni feeding when the
+            following condition is met:
+          </p>
+          <ul>
+            {filteredEvents.map((e, i) => {
+              return (
+                <li key={i}>
+                  <code>{JSON.stringify(e.condition)}</code>
+                </li>
+              );
+            })}
+          </ul>
+          <p>
+            See <Link to="/ryza3/puni_feeding">Puni feeding</Link> for more
+            info.
+          </p>
+        </>
+      )}
+    </>
+  );
 }
